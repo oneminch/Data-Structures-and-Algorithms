777731c2cdd707759f0e45119b60e8cd64d67fc6
oneminch <27751688+oneminch@users.noreply.github.com>
2024-07-08 18:58:11 -0400

    refactor: rewrite test for recursive binary search using different data types

--- java/src/test/java/io/github/oneminch/Algorithms/AlgorithmsTest.java
+++ java/src/test/java/io/github/oneminch/Algorithms/AlgorithmsTest.java
@@ -1,5 +1,6 @@
 package io.github.oneminch.Algorithms;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,27 +9,46 @@ import java.util.Collections;
 import java.util.List;
 
 public class AlgorithmsTest {
+    private static List<Integer> numList;
+    private static List<String> strList;
+    
+    @BeforeAll
+    static void setUp() {
+        numList = new ArrayList<>();
+        numList.add(31);
+        numList.add(71);
+        numList.add(14);
+        numList.add(21);
+        numList.add(35);
+        numList.add(12);
+        numList.add(51);
+        numList.add(61);
+        numList.add(19);
+        numList.add(10);
+        numList.add(39);
+        numList.add(77);
+        Collections.sort(numList);
+
+        strList = new ArrayList<>();
+        strList.add("John");
+        strList.add("Doe");
+        strList.add("Jane");
+        strList.add("Smith");
+        strList.add("Alice");
+        strList.add("Bob");
+        strList.add("David");
+        strList.add("Charlie");
+        Collections.sort(strList);
+    }
 
     @Test
-    public void testRecursiveBinarySearch() {
-        List<Integer> list = new ArrayList<>();
-        list.add(31);
-        list.add(71);
-        list.add(14);
-        list.add(21);
-        list.add(35);
-        list.add(12);
-        list.add(51);
-        list.add(61);
-        list.add(19);
-        list.add(10);
-        list.add(39);
-        list.add(77);
-        Collections.sort(list);
-
-        boolean actual = Algorithms.recursiveBinarySearch(list, 12);
-        boolean expected = true;
-
-        assertEquals(expected, actual);
+    void testRecursiveBinarySearch() {
+        // Integer Tests
+        assertTrue(Algorithms.recursiveBinarySearch(numList, 12));
+        assertFalse(Algorithms.recursiveBinarySearch(numList, 44));
+        
+        // String Tests
+        assertTrue(Algorithms.recursiveBinarySearch(strList, "Jane"));
+        assertFalse(Algorithms.recursiveBinarySearch(strList, "Elliot"));
     }
 }
