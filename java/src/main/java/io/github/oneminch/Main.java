358b1f4153c2041f25209abbab2c747750dbd93f
oneminch <27751688+oneminch@users.noreply.github.com>
2024-07-08 18:59:51 -0400

    refactor: remove manual testing code from entry file

--- java/src/main/java/io/github/oneminch/Main.java
+++ java/src/main/java/io/github/oneminch/Main.java
@@ -1,31 +1,8 @@
 package io.github.oneminch;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import io.github.oneminch.Algorithms.*;
-
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(31);
-        list.add(71);
-        list.add(14);
-        list.add(21);
-        list.add(35);
-        list.add(12);
-        list.add(51);
-        list.add(61);
-        list.add(19);
-        list.add(10);
-        list.add(39);
-        list.add(77);
-        Collections.sort(list);
-
-        if (Algorithms.recursiveBinarySearch(list, 12))
-            System.out.println("Found");
-        else
-            System.out.println("Not Found");
+        System.out.println("Data Structures & Algorithms");
+        System.out.println("Implemented in Java");
     }
 }
