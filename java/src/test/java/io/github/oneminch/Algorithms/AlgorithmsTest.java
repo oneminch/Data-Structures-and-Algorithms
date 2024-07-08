b7b39dccc80b3d00ffb591f3281530fb80168a4b
oneminch <27751688+oneminch@users.noreply.github.com>
2024-07-08 19:26:07 -0400

    feat: write tests for an iterative binary search algorithm

--- java/src/test/java/io/github/oneminch/Algorithms/AlgorithmsTest.java
+++ java/src/test/java/io/github/oneminch/Algorithms/AlgorithmsTest.java
@@ -15,29 +15,29 @@ public class AlgorithmsTest {
     @BeforeAll
     static void setUp() {
         numList = new ArrayList<>();
-        numList.add(31);
-        numList.add(71);
-        numList.add(14);
-        numList.add(21);
-        numList.add(35);
+        numList.add(10);
         numList.add(12);
+        numList.add(14);
+        numList.add(19);
+        numList.add(21);
+        numList.add(31);
+        numList.add(35);
+        numList.add(39);
         numList.add(51);
         numList.add(61);
-        numList.add(19);
-        numList.add(10);
-        numList.add(39);
+        numList.add(71);
         numList.add(77);
         Collections.sort(numList);
 
         strList = new ArrayList<>();
-        strList.add("John");
-        strList.add("Doe");
-        strList.add("Jane");
-        strList.add("Smith");
         strList.add("Alice");
         strList.add("Bob");
-        strList.add("David");
         strList.add("Charlie");
+        strList.add("David");
+        strList.add("Doe");
+        strList.add("Jane");
+        strList.add("John");
+        strList.add("Smith");
         Collections.sort(strList);
     }
 
@@ -45,10 +45,21 @@ public class AlgorithmsTest {
     void testRecursiveBinarySearch() {
         // Integer Tests
         assertTrue(Algorithms.recursiveBinarySearch(numList, 12));
-        assertFalse(Algorithms.recursiveBinarySearch(numList, 44));
+        assertFalse(Algorithms.recursiveBinarySearch(numList, 25));
         
         // String Tests
         assertTrue(Algorithms.recursiveBinarySearch(strList, "Jane"));
         assertFalse(Algorithms.recursiveBinarySearch(strList, "Elliot"));
+    }
+
+    @Test
+    void testIterativeBinarySearch() {
+        // Integer Tests
+        assertTrue(Algorithms.iterativeBinarySearch(numList, 12));
+        assertFalse(Algorithms.iterativeBinarySearch(numList, 25));
+        
+        // String Tests
+        assertTrue(Algorithms.iterativeBinarySearch(strList, "Jane"));
+        assertFalse(Algorithms.iterativeBinarySearch(strList, "Elliot"));
     }
 }
