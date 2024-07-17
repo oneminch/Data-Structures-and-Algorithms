c8b77e2479a996a36792e91cd8ea4ed5cff8f9d7
oneminch <27751688+oneminch@users.noreply.github.com>
2024-07-17 00:33:34 -0400

    feat: write tests for sorting algorithms
    - Bubble Sort
    - Quick Sort
    - Selection Sort

--- java/src/test/java/io/github/oneminch/Algorithms/AlgorithmsTest.java
+++ java/src/test/java/io/github/oneminch/Algorithms/AlgorithmsTest.java
@@ -12,36 +12,51 @@ import java.util.Collections;
 import java.util.List;
 
 public class AlgorithmsTest {
-    private static List<Integer> numList;
-    private static List<String> strList;
+    private static List<Integer> unsortedNumList;
+    private static List<Integer> sortedNumList;
+    private static List<String> sortedStrList;
 
     @BeforeAll
     static void setUp() {
-        numList = new ArrayList<>();
-        numList.add(10);
-        numList.add(12);
-        numList.add(14);
-        numList.add(19);
-        numList.add(21);
-        numList.add(31);
-        numList.add(35);
-        numList.add(39);
-        numList.add(51);
-        numList.add(61);
-        numList.add(71);
-        numList.add(77);
-        Collections.sort(numList);
+        unsortedNumList = new ArrayList<>();
+        unsortedNumList.add(51);
+        unsortedNumList.add(21);
+        unsortedNumList.add(35);
+        unsortedNumList.add(14);
+        unsortedNumList.add(31);
+        unsortedNumList.add(39);
+        unsortedNumList.add(12);
+        unsortedNumList.add(10);
+        unsortedNumList.add(77);
+        unsortedNumList.add(19);
+        unsortedNumList.add(71);
+        unsortedNumList.add(61);
 
-        strList = new ArrayList<>();
-        strList.add("Alice");
-        strList.add("Bob");
-        strList.add("Charlie");
-        strList.add("David");
-        strList.add("Doe");
-        strList.add("Jane");
-        strList.add("John");
-        strList.add("Smith");
-        Collections.sort(strList);
+        sortedNumList = new ArrayList<>();
+        sortedNumList.add(10);
+        sortedNumList.add(12);
+        sortedNumList.add(14);
+        sortedNumList.add(19);
+        sortedNumList.add(21);
+        sortedNumList.add(31);
+        sortedNumList.add(35);
+        sortedNumList.add(39);
+        sortedNumList.add(51);
+        sortedNumList.add(61);
+        sortedNumList.add(71);
+        sortedNumList.add(77);
+        Collections.sort(sortedNumList);
+
+        sortedStrList = new ArrayList<>();
+        sortedStrList.add("Alice");
+        sortedStrList.add("Bob");
+        sortedStrList.add("Charlie");
+        sortedStrList.add("David");
+        sortedStrList.add("Doe");
+        sortedStrList.add("Jane");
+        sortedStrList.add("John");
+        sortedStrList.add("Smith");
+        Collections.sort(sortedStrList);
     }
 
     @ParameterizedTest(name = "Fibonacci({0}) = {1}")
@@ -59,22 +74,88 @@ public class AlgorithmsTest {
     @Test
     void testRecursiveBinarySearch() {
         // Integer Tests
-        assertTrue(Algorithms.recursiveBinarySearch(numList, 12));
-        assertFalse(Algorithms.recursiveBinarySearch(numList, 25));
+        assertTrue(Algorithms.recursiveBinarySearch(sortedNumList, 12));
+        assertFalse(Algorithms.recursiveBinarySearch(sortedNumList, 25));
 
         // String Tests
-        assertTrue(Algorithms.recursiveBinarySearch(strList, "Jane"));
-        assertFalse(Algorithms.recursiveBinarySearch(strList, "Elliot"));
+        assertTrue(Algorithms.recursiveBinarySearch(sortedStrList, "Jane"));
+        assertFalse(Algorithms.recursiveBinarySearch(sortedStrList, "Elliot"));
     }
 
     @Test
     void testIterativeBinarySearch() {
         // Integer Tests
-        assertTrue(Algorithms.iterativeBinarySearch(numList, 12));
-        assertFalse(Algorithms.iterativeBinarySearch(numList, 25));
+        assertTrue(Algorithms.iterativeBinarySearch(sortedNumList, 12));
+        assertFalse(Algorithms.iterativeBinarySearch(sortedNumList, 25));
 
         // String Tests
-        assertTrue(Algorithms.iterativeBinarySearch(strList, "Jane"));
-        assertFalse(Algorithms.iterativeBinarySearch(strList, "Elliot"));
+        assertTrue(Algorithms.iterativeBinarySearch(sortedStrList, "Jane"));
+        assertFalse(Algorithms.iterativeBinarySearch(sortedStrList, "Elliot"));
+    }
+
+    @Test
+    void testSelectionSort() {
+        List<Integer> sortedList = new ArrayList<>();
+        sortedList.add(51);
+        sortedList.add(21);
+        sortedList.add(35);
+        sortedList.add(14);
+        sortedList.add(31);
+        sortedList.add(39);
+        sortedList.add(12);
+        sortedList.add(10);
+        sortedList.add(77);
+        sortedList.add(19);
+        sortedList.add(71);
+        sortedList.add(61);
+        sortedList.sort(null);
+
+        Object[] expected = sortedList.toArray();
+        Object[] actual = Algorithms.selectionSort(unsortedNumList).toArray();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testBubbleSort() {
+        List<Integer> sortedList = new ArrayList<>();
+        sortedList.add(51);
+        sortedList.add(21);
+        sortedList.add(35);
+        sortedList.add(14);
+        sortedList.add(31);
+        sortedList.add(39);
+        sortedList.add(12);
+        sortedList.add(10);
+        sortedList.add(77);
+        sortedList.add(19);
+        sortedList.add(71);
+        sortedList.add(61);
+        sortedList.sort(null);
+
+        Object[] expected = sortedList.toArray();
+        Object[] actual = Algorithms.bubbleSort(unsortedNumList).toArray();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testQuickSort() {
+        List<Integer> sortedList = new ArrayList<>();
+        sortedList.add(51);
+        sortedList.add(21);
+        sortedList.add(35);
+        sortedList.add(14);
+        sortedList.add(31);
+        sortedList.add(39);
+        sortedList.add(12);
+        sortedList.add(10);
+        sortedList.add(77);
+        sortedList.add(19);
+        sortedList.add(71);
+        sortedList.add(61);
+        sortedList.sort(null);
+
+        Object[] expected = sortedList.toArray();
+        Object[] actual = Algorithms.quickSort(unsortedNumList).toArray();
+        assertArrayEquals(expected, actual);
     }
 }
